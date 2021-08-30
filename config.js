f86c5fc21bcf3230ddda17b24f32655e027a60fc
rahul-v-prijeesh <76157423+rahul-v-prijeesh@users.noreply.github.com>
2021-08-30 15:11:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,14 @@ require("@firebase/firestore")
 
 const firebaseConfig = {
  //Add configuration here
- 
+
+  apiKey: "AIzaSyBxUgV-Ec65YnwGAiAAINVVjte1qN5MKno",
+  authDomain: "project-71-fc6c2.firebaseapp.com",
+  projectId: "project-71-fc6c2",
+  storageBucket: "project-71-fc6c2.appspot.com",
+  messagingSenderId: "4884655494",
+  appId: "1:4884655494:web:d20bf379d1ba5c9ebdd007"
+
 };
   firebase.initializeApp(firebaseConfig);
 
